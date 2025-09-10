37c5adee1efd65cf847a05ffcfb6b00528936e4a
Victor Pontes <149399268+vppontes@users.noreply.github.com>
2025-09-10 20:55:06 -0300

    Criação de perguntas com IA
    Geração de perguntas com IA do Google Gemini
    Inserção no banco de dados para o projeto Quizzando (trabalho escolar)

---
+++ questions.py
@@ -0,0 +1,98 @@
+import json
+from google import genai
+import psycopg2
+import re
+from dotenv import load_dotenv
+from disciplines import disciplines
+import os
+
+load_dotenv()
+
+# API Key
+apiKey = os.getenv("API_KEY")
+
+# Conexão com o banco
+dbname = os.getenv("DB_NAME")
+host = os.getenv("DB_HOST")
+port = int(os.getenv("DB_PORT"))
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+
+client = genai.Client(api_key=apiKey)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents='''
+        Quero que elabore perguntas de quizz, com 5 alternativas, níveis diferentes sobre o ensino de ADS, que tem as disciplinas Banco de Dados, Programação Web, Qualidade e Teste de Software, Segurança da Informação, Design Digital, Sistemas Embarcados, Programação Mobile, Fundamentos da Informática, Análise e Projeto de Sistemas, Técnicas de Programação e Algoritmos e volte um JSON da seguinte forma:
+        [
+            {
+                "question_title": "(Título da questão de no máximo 140 caracteres)",
+                "discipline": "(disciplina da pergunta)",
+                "alternatives": [["(texto da alternativa de no máximo 90 caracteres)", (1 para correta ou 0 para incorreta)]],
+                "difficulty": "(um número de 0 à 2 conforme a dificuldade da pergunta)"
+            }
+        ]
+    '''
+)
+
+response_text = response.text
+
+clean_text = re.sub(r'^```json', '', response_text)
+clean_text = re.sub(r'```', '', clean_text)
+
+clean_text = clean_text.strip()
+
+questions = json.loads(clean_text)
+
+print('Questões:\n', questions)
+
+def insert_questions(q):
+    if not q:
+        print('Questões não foram criadas corretamente!')
+        return
+
+    con = psycopg2.connect(
+        dbname = dbname,
+        host = host,
+        port = port,
+        user = user,
+        password = password
+    )
+    cursor = con.cursor()
+
+    for question in q:
+        title = question["question_title"]
+        discipline = question["discipline"]
+        difficulty = question["difficulty"]
+        alternatives = question["alternatives"]
+
+        discipline_id = disciplines.get(discipline, None)
+        if not discipline_id:
+            print(f'Disciplina "{discipline}" não encontrada. Pulando questão.')
+            continue
+
+        sql_insert_question = '''
+            INSERT INTO quizz."Question"
+            ("Id", "QuestionStatement", "DisciplineId", "Difficulty")
+            VALUES(gen_random_uuid(), %s, %s, %s)
+            RETURNING "Id";
+        '''
+        cursor.execute(sql_insert_question, (title, discipline_id, difficulty))
+
+        question_id = cursor.fetchone()[0]
+
+        for text_alternative, correct in alternatives:
+            correct = True if correct == 1 else False
+            sql_insert_answers = '''
+                INSERT INTO quizz."Answer"
+                ("Id", "QuestionId", "AnswerText", "IsCorrect")
+                VALUES(gen_random_uuid(), %s, %s, %s)
+            '''
+            cursor.execute(sql_insert_answers, (question_id, text_alternative, correct))
+
+    con.commit()
+    cursor.close()
+    con.close()
+    print("Todas as questões e alternativas foram inseridas com sucesso!")
+
+insert_questions(questions)
